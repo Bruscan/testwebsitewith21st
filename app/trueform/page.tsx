6303d21b9a4f7cb41721b2ce97e03e365d8042ec
Bruscan <bga2322@gmail.com>
2025-12-03 19:13:42 +0100

    Use window.location.href instead of window.open

--- app/trueform/page.tsx
+++ app/trueform/page.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 
 export default function TrueFormPage() {
   const handleAppStoreClick = async () => {
-    window.open('https://apps.apple.com/app/apple-store/id6745799401?ct=web&mt=8', '_blank');
+    window.location.href = 'https://apps.apple.com/app/apple-store/id6745799401?ct=web&mt=8';
   };
 
   return (
